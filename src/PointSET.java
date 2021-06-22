9b18c4911438216822f3e8de4aea8b3cce73bae8
Nikita <sotheres@gmail.com>
2021-06-22 14:03:05 +0300

    Initial commit

---
+++ src/PointSET.java
@@ -0,0 +1,44 @@
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+import java.util.TreeSet;
+
+public class PointSET {
+    
+    TreeSet<Point2D> set;
+
+    public PointSET() {
+
+    }
+
+    public boolean isEmpty() {
+
+    }
+
+    public int size() {
+
+    }
+
+    public void insert(Point2D p) {
+
+    }
+
+    public boolean contains(Point2D p) {
+
+    }
+
+    public void draw() {
+
+    }
+
+    public Iterable<Point2D> range(RectHV rect) {
+
+    }
+
+    public Point2D nearest(Point2D p) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
